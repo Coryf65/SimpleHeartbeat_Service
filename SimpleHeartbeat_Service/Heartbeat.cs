ac9cb6225b5ff5af6e6c7ad99d8b37428bebbb52
Cory Fabian <Coryf65@gmail.com>
2022-01-05 19:34:58 -0600

    Add project files.

---
+++ SimpleHeartbeat_Service/Heartbeat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+
+namespace SimpleHeartbeat_Service
+{
+    public class Heartbeat
+    {
+        // what is this going to do
+        // timer every sec and write to a file
+
+        private readonly System.Timers.Timer _heartbeatTimer;
+
+        public Heartbeat()
+        {
+            _heartbeatTimer = new System.Timers.Timer(1000) { AutoReset = true };
+            _heartbeatTimer.Elapsed += TimerElapsed;
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ SimpleHeartbeat_Service/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Event Driven
+
+Console.WriteLine("Hello, World!");
+
+
+// Service Code
+
